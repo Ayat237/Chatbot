b6fc0798eed71d45c6bfcddee93dc4bcc863395d
Ayat Mohamed <109925260+Ayat237@users.noreply.github.com>
2024-08-08 16:35:23 +0300

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import interactionRouter from "./src/modules/interactions/interaction.routes.js"
 config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 
 app.use(express.json());
 app.use("/chat", chatRouter);
