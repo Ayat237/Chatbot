c99125bc5231cc845570b70fc6e7cae51b2f66ac
Ayat Mohamed <amohamed@darkmon.com>
2026-05-17 23:06:21 +0300

    export createApp for netlify

--- index.js
+++ index.js
@@ -9,23 +9,30 @@ import interactionRouter from "./src/modules/interactions/interaction.routes.js"
 
 config();
 
-const app = express();
-const port = process.env.port || 5000;
+export const createApp = () => {
+  const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use("/chat", chatRouter);
-app.use("/analatyics", interactionRouter);
-app.use("/", (req, res, next) =>
-  res.status(200).json({
-    status: "success",
-    message: "Server is running",
-  })
-);
-app.use("/*", (req, res, next) => {
-  return next(new ErrorClass(`Invalid URL : ${req.originalUrl}`, 404));
-});
+  app.use(cors());
+  app.use(express.json());
+  app.use("/chat", chatRouter);
+  app.use("/analatyics", interactionRouter);
+  app.use("/", (req, res, next) =>
+    res.status(200).json({
+      status: "success",
+      message: "Server is running",
+    })
+  );
+  app.use("/*", (req, res, next) => {
+    return next(new ErrorClass(`Invalid URL : ${req.originalUrl}`, 404));
+  });
 
-app.use(globaleResponse);
+  app.use(globaleResponse);
 
-app.listen(port, () => console.log(`app listening on port ${port}!`));
+  return app;
+};
+
+if (!process.env.NETLIFY) {
+  const port = process.env.PORT || 5000;
+  const app = createApp();
+  app.listen(port, () => console.log(`app listening on port ${port}!`));
+}
